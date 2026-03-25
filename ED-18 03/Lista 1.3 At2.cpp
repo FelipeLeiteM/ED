53b812656a561b57d09311ac5c49b245624b8f60
FelipeLeiteM <felipegabrielgod666@gmail.com>
2026-03-25 07:02:08 -0300

    Atualizar o Lista 1.3 At2.cpp

--- ED-18 03/Lista 1.3 At2.cpp
+++ ED-18 03/Lista 1.3 At2.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include <iostream>
-#include <list>
-
 using namespace std;
 
-int main()
-{
-	
-	setlocale(LC_ALL, "portuguese");
-	
-	list<string> numlist = {"1", "2", "3", "4", "5"};
-	
-	
-	for(int i=4; i>=0; i--){
-	for(auto element: numlist){
+int main() {
+    string lista[5];
 
-cout << numlist[i];
+    cout << "Digite 5 palavras:\n";
+    for (int i = 0; i < 5; i++) {
+        cin >> lista[i];
+    }
 
-	}
+    cout << "\nOrdem invertida:\n";
+    for (int i = 4; i >= 0; i--) {
+        cout << lista[i] << endl;
+    }
 
-	return 0;
-}
+    return 0;
 }
