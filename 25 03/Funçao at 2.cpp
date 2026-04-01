ecb98f886d671925dbe250bc927fd23bd139dd85
FelipeLeiteM <felipegabrielgod666@gmail.com>
2026-04-01 08:06:53 -0300

    Update Funçao at 2.cpp

--- 25 03/Funçao at 2.cpp
+++ 25 03/Funçao at 2.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-bool ehPar(int numero){
+bool Par(int numero){
 	return numero % 2 == 0;
 }
 
 int main(){
 	int numero = 7;
 	
-	if (ehPar(numero)){
+	if (Par(numero)){
 		cout << numero << "é numero par" << endl;
 	} else {
 		cout << numero << "não é um número par" << endl;
